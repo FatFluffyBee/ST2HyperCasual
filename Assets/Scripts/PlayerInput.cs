e9e0244cb4a31261d2605f1964d02c60b7cd9c33
FatFluffyBee <corentinvignaud@gmail.com>
2022-02-16 14:11:08 +0100

    Fix arrow + feedback

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerInput : MonoBehaviour
 {
     public GameObject arrow, pivotArrow;
     public AnimationCurve slowMoCurve, ballSpeedCurve;
+    public Gradient gradient;
     public float timeToSlowMo, timeToReachMinSpeed;
-    public float maxSpeed, forceRotation;
-    public float maxDistanceForDrag, maxArrowScale;
+    public float maxSpeed, minSpeed, forceRotation;
+    public float maxDragScreen, maxArrowScale;
 
-    private float timeToSlowMoCount, timeToReachMinSpeedCount;
+    private float timeToSlowMoCount, timeToReachMinSpeedCount, maxDistanceForDrag;
     private Rigidbody rB;
-    private GameObject startPoint, endPoint;
-    private float releaseSpeed;
+    private Image startPoint, endPoint;
+    private float releaseSpeed, minArrowScale;
+    private SpriteRenderer arrowRd;
 
     // Start is called before the first frame update
     void Start()
@@ -21,11 +24,24 @@ public class PlayerInput : MonoBehaviour
         Screen.SetResolution(1080, 2400, true);
         rB = GetComponent<Rigidbody>();
 
-        startPoint = GameObject.Find("StartPoint");
-        endPoint = GameObject.Find("EndPoint");
+        startPoint = GameObject.Find("StartPoint").GetComponent<Image>();
+        endPoint = GameObject.Find("EndPoint").GetComponent<Image>();
+        arrowRd = arrow.GetComponent<SpriteRenderer>();
+
+        minArrowScale = minSpeed * maxArrowScale / maxSpeed;
+
+        startPoint.transform.localScale = Vector3.one * Screen.width / 400f;
+        endPoint.transform.localScale = Vector3.one * Screen.width / 400f * 0.6f;
+
+        maxDistanceForDrag = Screen.width * maxDragScreen;
     }
 
     // Update is called once per frame
+
+    private void FixedUpdate()
+    {
+        
+    }
 
     void Update()
     {
@@ -33,9 +49,9 @@ public class PlayerInput : MonoBehaviour
         {
             if (Input.GetTouch(0).phase == TouchPhase.Began)
             {
-                startPoint.SetActive(true);
-                endPoint.SetActive(true);
-                arrow.SetActive(true);
+                startPoint.enabled = true;
+                endPoint.enabled = true;
+                arrowRd.enabled = true;
                 Vector3 positionStartPoint = Input.GetTouch(0).position;
                 startPoint.transform.position = positionStartPoint;
             }
@@ -48,9 +64,10 @@ public class PlayerInput : MonoBehaviour
                     endPoint.transform.position = startPoint.transform.position + (tmp - startPoint.transform.position).normalized * maxDistanceForDrag;}
                 else { endPoint.transform.position = tmp;}
 
-                Vector3 arrowScale = arrow.transform.localScale;
-                arrowScale.y = Vector3.Distance(endPoint.transform.position, startPoint.transform.position) / maxDistanceForDrag * maxArrowScale;
+                Vector3 arrowScale = Vector3.one;
+                arrowScale.y = Vector3.Distance(endPoint.transform.position, startPoint.transform.position) / maxDistanceForDrag * (maxArrowScale - minArrowScale) + minArrowScale;
                 arrow.transform.localScale = arrowScale;
+                arrowRd.color = gradient.Evaluate((arrowScale.y - minArrowScale) / (maxArrowScale - minArrowScale));
 
                 Vector3 direction = endPoint.transform.position - startPoint.transform.position;
                 direction.z = direction.y;
@@ -59,7 +76,6 @@ public class PlayerInput : MonoBehaviour
                 Vector3 arrowPosition = Vector3.zero;
                 arrowPosition.z = arrow.transform.localScale.y;
                 arrow.transform.position = transform.position - direction.normalized * arrow.transform.localScale.y;
-
                 
                 pivotArrow.transform.LookAt(transform.position - direction.normalized, Vector3.up);
 
@@ -68,20 +84,25 @@ public class PlayerInput : MonoBehaviour
 
             if (Input.GetTouch(0).phase == TouchPhase.Ended)
             {
-                startPoint.SetActive(false);
-                endPoint.SetActive(false);
-                arrow.SetActive(false);
+                startPoint.enabled = false;
+                endPoint.enabled = false;
+                arrowRd.enabled = false;
 
                 Vector3 direction = startPoint.transform.position - endPoint.transform.position;
-                direction = direction.normalized;
-                direction.z = direction.y;
-                direction.y = 0;
+
+                if (direction != Vector3.zero)
+                {
+                    direction = direction.normalized;
+                    direction.z = direction.y;
+                    direction.y = 0;
+                    
+
+                    releaseSpeed = Vector3.Distance(endPoint.transform.position, startPoint.transform.position) / maxDistanceForDrag * (maxSpeed - minSpeed) + minSpeed;
+                    rB.velocity = direction * releaseSpeed;
+
+                    timeToReachMinSpeedCount = 0;
+                }
                 StopSlowMo();
-
-                releaseSpeed = Vector3.Distance(endPoint.transform.position, startPoint.transform.position) / maxDistanceForDrag * maxSpeed;
-                rB.velocity = direction * releaseSpeed;
-
-                timeToReachMinSpeedCount = 0;
             }
         }
 
